7ca7a02e8830d89876dd2dd62539a019494a01ea
ajayjain62 <ajay8290440876@gmail.com>
2024-12-10 18:28:35 +0530

    changes

--- src/components/Footer/ItemsContainer.jsx
+++ src/components/Footer/ItemsContainer.jsx
@@ -1,9 +1,9 @@
 import Item from "./Item";
-import { PRODUCTS, RESOURCES, COMPANY, SUPPORT } from "./Menus";
+import { QUICK_LINKS, RESOURCES, COMPANY, SUPPORT } from "./Menus";
 const ItemsContainer = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
-      <Item Links={PRODUCTS} title="PRODUCTS" />
+      <Item Links={QUICK_LINKS} title="QUICK LINKS" />
       <Item Links={RESOURCES} title="RESOURCES" />
       <Item Links={COMPANY} title="COMPANY" />
       <Item Links={SUPPORT} title="SUPPORT" />
--- src/components/Footer/Menus.js
+++ src/components/Footer/Menus.js
@@ -1,33 +1,38 @@
-export const PRODUCTS = [
-  { name: "Drag And Drop", link: "#" },
-  { name: "Visual Studio X", link: "#" },
-  { name: "Easy Content", link: "#" },
+export const QUICK_LINKS = [
+  { name: "New Arrivals", link: "/new-arrivals" },
+  { name: "Best Sellers", link: "/best-sellers" },
+  { name: "On Sale", link: "/on-sale" },
+  { name: "Gift Cards", link: "/gift-cards" },
 ];
+
 export const RESOURCES = [
-  { name: "Industries and tools", link: "#" },
-  { name: "Use cases", link: "#" },
-  { name: "Blog", link: "#" },
-  { name: "Online evenet", link: "#" },
-  { name: "Nostrud exercitation", link: "#" },
+  { name: "About Us", link: "/about-us" },
+  { name: "Sustainability", link: "/sustainability" },
+  { name: "Artisan Stories", link: "/artisan-stories" },
+  { name: "Blog", link: "/blog" },
+  { name: "Care Instructions", link: "/care-instructions" },
 ];
+
 export const COMPANY = [
-  { name: "Diversity & inclusion", link: "#" },
-  { name: "About us", link: "#" },
-  { name: "Press", link: "#" },
-  { name: "Customer Stories", link: "#" },
-  { name: "Online communities", link: "#" },
+  { name: "Our Story", link: "/our-story" },
+  { name: "Customer Reviews", link: "/reviews" },
+  { name: "Press & Media", link: "/press-media" },
+  { name: "Careers", link: "/careers" },
+  { name: "Affiliate Program", link: "/affiliate-program" },
 ];
+
 export const SUPPORT = [
-  { name: "Documentation", link: "#" },
-  { name: "Tutorials & guides", link: "#" },
-  { name: "Webinars", link: "#" },
-  { name: "Open-source", link: "#" },
+  { name: "FAQs", link: "/faqs" },
+  { name: "Shipping & Returns", link: "/shipping-returns" },
+  { name: "Track Your Order", link: "/track-order" },
+  { name: "Contact Us", link: "/contact" },
+  { name: "Privacy Policy", link: "/privacy-policy" },
 ];
 
 export const Icons = [
-  { name: "logo-facebook", link: "#" },
-  { name: "logo-twitter", link: "#" },
-  { name: "logo-github", link: "#" },
-  { name: "logo-linkedin", link: "#" },
-  { name: "logo-instagram", link: "#" },
+  { name: "logo-facebook", link: "https://www.facebook.com/gulabidecor" },
+  { name: "logo-twitter", link: "https://www.twitter.com/gulabidecor" },
+  { name: "logo-instagram", link: "https://www.instagram.com/gulabidecor" },
+  { name: "logo-pinterest", link: "https://www.pinterest.com/gulabidecor" },
+  { name: "logo-youtube", link: "https://www.youtube.com/gulabidecor" },
 ];
